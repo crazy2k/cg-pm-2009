10fdbc8585e7a32ce5b205ec251c121bd4db459c
pabloa@gmail.com <pabloa@gmail.com@e377ba90-2068-11de-a7a1-25a91d9625e5>
2009-04-03 23:32:36 +0000

    Van los archivos para empezar con el repo.

---
+++ TP1/src/dibujar_lineas.py
@@ -0,0 +1,30 @@
+import wx
+
+import bresenham
+import dda
+import scan
+
+class Ventana(wx.Frame):
+    def __init__(self, id, title, algoritmos):
+        wx.Frame.__init__(self, None, id, title, size=(500, 600))
+
+        self.algoritmos = algoritmos
+
+        self.Bind(wx.EVT_PAINT, self.dibujar_linea)
+
+        self.Centre()
+        self.Show(True)
+
+
+    def dibujar_linea(self, event):
+        dc = wx.ClientDC(self)
+        self.algoritmos.dibujar_segmento(10, 10, 400, 100, dc.DrawPoint)
+        self.algoritmos.dibujar_segmento(400, 500, 10, 10, dc.DrawPoint)
+        self.algoritmos.dibujar_segmento(100, 500, 400, 10, dc.DrawPoint)
+        self.algoritmos.dibujar_segmento(400, 10, 10, 500, dc.DrawPoint)
+        scan.scan_triangle(10,10,400,100,450,500,self.algoritmos.dibujar_segmento, dc.DrawPoint)
+
+app = wx.App()
+Ventana(id = 0, title = "Lineas con Bresenham", algoritmos = bresenham)
+Ventana(id = 1, title = "Lineas con DDA", algoritmos = dda)
+app.MainLoop()
