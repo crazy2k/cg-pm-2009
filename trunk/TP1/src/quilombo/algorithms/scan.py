79b207fe9e7654bdab6c008d2867d544a5bbf49e
pabloa <pabloa@e377ba90-2068-11de-a7a1-25a91d9625e5>
2009-04-11 09:43:02 +0000

    Ahora podemos dibujar polígonos convexos (no sólo triángulos) con el algoritmo de scan. Faltaría hacer que los triángulos se dibujen con este nuevo algoritmo: Hay que cambiar, en todos lados, que en vez de pasar 3 parámetros al algoritmo (uno por cada vértice) se pase uno solo que es la lista de vértices.

--- trunk/TP1/src/quilombo/algorithms/scan.py
+++ trunk/TP1/src/quilombo/algorithms/scan.py
@@ -4,23 +4,16 @@ class TriangleScanAlgorithm:
         self.__maxx = 0
         self.__maxy = 0
 
-    def scan(self, vertex1, vertex2, vertex3, draw_segment, putpixel, colour):
-        x1 = vertex1[0]
-        y1 = vertex1[1]
-        x2 = vertex2[0]
-        y2 = vertex2[1]
-        x3 = vertex3[0]
-        y3 = vertex3[1]
-
-        fig_maxwidth = max(x1, x2, x3) + 1
-        fig_maxheight = max(y1, y2, y3) + 1
+    def scan(self, vertices, draw_segment, putpixel, colour):
+        fig_maxwidth = max([v[0] for v in vertices]) + 1
+        fig_maxheight = max([v[1] for v in vertices]) + 1
 
         self.__maxx = [-1]*fig_maxheight
         self.__minx = [fig_maxwidth]*fig_maxheight
 
-        draw_segment((x1, y1), (x2, y2), self.__max_setter, None)
-        draw_segment((x2, y2), (x3, y3), self.__max_setter, None)
-        draw_segment((x1, y1), (x3, y3), self.__max_setter, None)
+        for i in range(len(vertices) - 1):
+            draw_segment(vertices[i], vertices[i+1], self.__max_setter, None)
+        draw_segment(vertices[-1], vertices[0], self.__max_setter, None)
 
         for y in range(fig_maxheight):
             if self.__maxx[y] != -1:
