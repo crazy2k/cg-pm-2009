acb546783413d1c7e7c6ddcf3f6647dce4145437
pabloa <pabloa@e377ba90-2068-11de-a7a1-25a91d9625e5>
2009-04-18 06:57:43 +0000

    Se ve medio feo porque anduve toqueteando. Probé dos maneras nuevas de generar el bmp, pero ninguna mejoró la performance.
    Eso es bueno desde el punto de vista de que había elegido sin saberlo
    del todo la mejor alternativa. Pero es malo porque quiere decir que
    estamos usando el límite de la performance.
    
    Quizás habría que buscar optimizar por otro lado...

--- trunk/TP1/src/quilombo/core/windows.py
+++ trunk/TP1/src/quilombo/core/windows.py
@@ -2,6 +2,9 @@ import wx
 from utils import convert
 from PIL import Image
 
+#import array
+import numpy
+
 class GenericWindow(wx.Frame):
     
     AUTO_REFRESHING = 0
@@ -38,17 +41,45 @@ class GenericWindow(wx.Frame):
     def on_paint(self, event):
 
         size = self.GetSize().Get()
+
+        width = size[0]
+        height = size[1]
+
         # creo una imagen de PIL, en blanco
-        im = Image.new('RGB', size, (255, 255, 255))
+        #im = Image.new('RGB', size, (255, 255, 255))
 
         # voy a acceder a sus pixels a traves de esta variable
-        self.__buff = im.load()
+        #self.__buff = im.load()
+
+        #bmp = wx.EmptyBitmap(size[0], size[1], 32)
+
+        #self.__px_data = wx.NativePixelData(bmp)
+        #print wx.NativePixelData(bmp)
+        #if not self.__px_data:
+        #    raise RuntimeError("No se puede acceder a los datos del bmp.")
+
+        #self.__pixels = self.__px_data.GetPixels()
+        #for y in xrange(size[1]):
+        #    for x in xrange(size[0]):
+        #        pixels.Set
+        # Make a bitmap using an array of RGB bytes
+
+        self.__bpp = 3  # bytes per pixel
+        #self.__bytes = array.array('B', [255] * width*height*self.__bpp)
+        self.__bytes = numpy.zeros((width, height, 3), 'uint8')
+
 
         # escribo en sus pixels, segun la funcion de dibujado
         self.draw(self.add_to_buffer)
 
         # me genero un bitmap a partir de la imagen creada
-        bmp = convert.pilToBitmap(im)
+        #bmp = convert.pilToBitmap(im)
+
+        #bmp = wx.BitmapFromBuffer(width, height, self.__bytes)
+        
+        image = wx.EmptyImage(width, height)
+        image.SetData(self.__bytes.tostring())
+        bmp = image.ConvertToBitmap()
 
         # pinto el bitmap en la ventana
         pdc = wx.PaintDC(self)
@@ -58,9 +89,24 @@ class GenericWindow(wx.Frame):
     def add_to_buffer(self, x, y, colour = (0, 0, 0)):
         size = self.GetSize().Get()
 
+        width = size[0]
+        height = size[1]
+
         x = x % size[0]
         y = y % size[1]
-        self.__buff[x, y] = colour
+
+        #self.__buff[x, y] = colour
+
+        #self.__pixels.MoveTo(self.__px_data, x, y)
+        #self.__pixels.Set(colour[0], colour[1], colour[2])
+
+        #offset = y*width*self.__bpp + x*self.__bpp
+        #r, g, b = colour[0], colour[1], colour[2]
+        #self.__bytes[offset + 0] = r
+        #self.__bytes[offset + 1] = g
+        #self.__bytes[offset + 2] = b
+        self.__bytes[x][y] = [colour[0], colour[1], colour[2]]
+
 
     def draw(self, x, y):
         raise NotImplementedError
