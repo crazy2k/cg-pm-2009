2a40ae99786f659328212c1999ede2db20f14bb2
martaponzoni@gmail.com <martaponzoni@gmail.com@e377ba90-2068-11de-a7a1-25a91d9625e5>
2009-04-22 06:00:52 +0000

    Hago commit desde el usuario de Marta por si hay cambios importantes. Ahora me paso al mío. (Pablo)

--- TP1/src/quilombo/algorithms/windowing.py
+++ TP1/src/quilombo/algorithms/windowing.py
@@ -11,8 +11,8 @@ def WindowingMatrix(old_viewport, new_viewport):
     yva = new_viewport.getRefPoint()[1]
     yvb = new_viewport.getRefPoint()[1] + new_viewport.getHeight()
     
-    fx = (xvd - xvi)/(xwd - xwi)
-    fy = (yva - yvb)/(ywa - ywb)
+    fx = float((xvd - xvi))/(xwd - xwi)
+    fy = float((yva - yvb))/(ywa - ywb)
     tx = xvi - fx*xwi
     ty = yva - fy*ywa
     windowing_matrix = [[fx, 0, tx], [0, fy, ty], [0, 0, 1]]
