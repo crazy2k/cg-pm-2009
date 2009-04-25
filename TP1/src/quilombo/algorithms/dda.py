7628bfe741b944df24d8905cad950c8259aede4b
pabloa <pabloa@e377ba90-2068-11de-a7a1-25a91d9625e5>
2009-04-25 21:58:42 +0000

    Algoritmo de DDA arreglado. Ahora dibuja, además de sin huecos, correctamente.
    Lo probamos en lugar de DDA para la escena principal y se ve igual.
    
    También probamos la performance y vimos que DDA fue casi el doble
    de lento.

--- TP1/src/quilombo/algorithms/dda.py
+++ TP1/src/quilombo/algorithms/dda.py
@@ -21,34 +21,31 @@ def draw_segment(endpoint1, endpoint2, putpixel, colour):
 
         return
 
+    # si la pendiente tiene modulo mayor que 1, lo que vamos a hacer es
+    # calcular la ubicacion para los puntos reflejados del otro lado de la
+    # recta y = x, pero dibujaremos los pixels en el lugar en el que van
+    # (mas adelante, imprimimos (y, x) en lugar de (x, y)
+    steep = abs(y2 - y1) > abs(x2 - x1)
+    if steep:
+        x1, y1 = y1, x1
+        x2, y2 = y2, x2
+
+    if x1 > x2:
+        x1, x2 = x2, x1
+        y1, y2 = y2, y1
+
     dx = x2 - x1
     dy = y2 - y1
 
-    m = float((y2 - y1))/(x2 - x1)
-        
-    if -1 <= m <= 1:
-        
-        if x1 > x2:
-            x1, x2 = x2, x1
-            y1, y2 = y2, y1
-        
-        y = y1
+    m = float(dy)/dx
 
-        for x in range(x1, x2 -1):
-            putpixel(x, round(y), colour)
-            y = y + m
+    y = y1
 
-    else:
-        
-        if y1 < y2:
-            x1, x2 = x2, x1
-            y1, y2 = y2, y1
-        
-        x = x1
-        y = y1
-        
-        while y >= y2 -1:
+    for x in range(x1, x2 + 1):
+        if steep:
+            putpixel(int(round(y)), x, colour)
+        else:
+            putpixel(x, int(round(y)), colour)
+        y = y + m
 
-            putpixel(round(x), y, colour)
-            x = x - 1/m
-            y = y - 1
+   
