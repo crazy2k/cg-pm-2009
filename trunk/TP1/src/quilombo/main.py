92728e4b6ff6a828ce1509c3c82771ba14f8dd4d
pabloa <pabloa@e377ba90-2068-11de-a7a1-25a91d9625e5>
2009-05-18 05:57:13 +0000

    Modos editor y dibujos.

--- trunk/TP1/src/quilombo/main.py
+++ trunk/TP1/src/quilombo/main.py
@@ -24,7 +24,13 @@ class MyButton(wx.Button):
         self.window = window
 
         wx.EVT_LEFT_DOWN(self, self.OnMouseDown)
+        wx.EVT_RIGHT_DOWN(self, self.OnMouseRightDown)
 
+
+    def OnMouseRightDown(self, event):
+        if self.window.being_dragged == None:
+            del(self.window.c_points[self.id])
+            self.window.Refresh()
 
     def OnMouseDown(self, event):
         if self.window.being_dragged == None:
@@ -42,15 +48,14 @@ class CurvesWindow(GenericWindow):
         wx.EVT_MOTION(self, self.OnMouseMove)
         wx.EVT_LEFT_DOWN(self, self.OnMouseDown)
 
-
         self.CreateMenu()
 
         #self.c_points = [[100,100],[150,80],[200,100], [200,150], [150,180],[100,150],[100,100],[150,80],[200,100], [200,150], [150,180],[100,150],[100,100],[150,80],[200,100], [200,150], [150,180],[100,150]]
         self.c_points = []
 
-        self.count = 0
         self.being_dragged = None
         self.buttons = []
+        self.show_drawings = False
 
 
     def OnMouseDown(self, event):
@@ -76,8 +81,12 @@ class CurvesWindow(GenericWindow):
         menu_algo.Append(3, "B-Spline No Uniforme")
 
         menu_opt = wx.Menu()
-        menu_opt.Append(5, "Limpiar")
+        menu_opt.Append(5, "Limpiar editor")
+        menu_opt.AppendSeparator()
         menu_opt.Append(6, "Imprimir puntos del grafo de control")
+        menu_opt.AppendSeparator()
+        menu_opt.Append(7, "Mostrar/Ocultar dibujos")
+
         
         menuBar = wx.MenuBar()
         menuBar.Append(menu_algo, "Algoritmos")
@@ -89,7 +98,17 @@ class CurvesWindow(GenericWindow):
         self.Bind(wx.EVT_MENU, self.menuBezierMenorGrado, id=4)
         self.Bind(wx.EVT_MENU, self.menuLimpiar, id=5)
         self.Bind(wx.EVT_MENU, self.menuImprimir, id=6)
+        self.Bind(wx.EVT_MENU, self.menuMostrarDibujos, id=7)
         
+    def menuMostrarDibujos(self, event):
+        if self.show_drawings:
+            self.show_drawings = False
+        else:
+            self.show_drawings = True
+
+        self.Refresh()
+
+
     def menuImprimir(self, event):
         print self.c_points
 
@@ -115,35 +134,45 @@ class CurvesWindow(GenericWindow):
     
     def draw(self, putpixel):
 
-        self.count += 1
+        if self.show_drawings:
+            nube = [[313, 410], [305, 486], [205, 486], [185, 412], [95, 434],
+                [48, 389], [100, 336], [45, 295], [46, 231], [117, 213],
+                [132, 149], [196, 142], [236, 203], [291, 135], [371, 138],
+                [370, 205], [445, 146], [510, 139], [540, 210], [616, 204],
+                [660, 257], [577, 312], [663, 353], [647, 397], [582, 418],
+                [577, 488], [502, 487], [490, 428], [423, 513], [365, 509],
+                [322, 410]]
+            curvas.BezierMenorGrado(nube, 1000, bresenham.draw_segment, putpixel)
 
-        c_points = self.c_points
+        else:
 
-        for b in self.buttons:
-            b.Destroy()
+            c_points = self.c_points
 
-        self.buttons = []
-        s = len(c_points)
-    
-        b_size = (15, 15)
+            for b in self.buttons:
+                b.Destroy()
 
-        alter_size = lambda p: [p[0] - b_size[0]/2, p[1] - b_size[1]/2]
+            self.buttons = []
+            s = len(c_points)
+        
+            b_size = (15, 15)
 
-        for i in range(s):
-            if i + 1 != s:
-                LineSegment([int(c_points[i][0]),int(c_points[i][1])], [int(c_points[i+1][0]),int(c_points[i+1][1])] , bresenham.draw_segment, (255, 0, 0)).draw(putpixel)
+            alter_size = lambda p: [p[0] - b_size[0]/2, p[1] - b_size[1]/2]
 
-            self.buttons.append(MyButton(self, i, '', alter_size(c_points[i]), b_size, c_points[i]))
+            for i in range(s):
+                if i + 1 != s:
+                    LineSegment([int(c_points[i][0]),int(c_points[i][1])], [int(c_points[i+1][0]),int(c_points[i+1][1])] , bresenham.draw_segment, (255, 0, 0)).draw(putpixel)
 
-        if self.__algorithm == "Bezier":
-            curvas.Bezier(c_points, 1000, bresenham.draw_segment, putpixel)
-        elif self.__algorithm == "BSplineUniforme":
-            curvas.bsplines(c_points, 1000, bresenham.draw_segment, putpixel)
-        elif self.__algorithm == "BSplineNoUniforme":
-            curvas.bsplines_no_uniforme(c_points, 1000, 3, bresenham.draw_segment, putpixel)
-        elif self.__algorithm == "BezierMenorGrado": 
-            curvas.BezierMenorGrado(c_points, 1000, bresenham.draw_segment, putpixel)
-  
+                self.buttons.append(MyButton(self, i, '', alter_size(c_points[i]), b_size, c_points[i]))
+
+            if self.__algorithm == "Bezier":
+                curvas.Bezier(c_points, 1000, bresenham.draw_segment, putpixel)
+            elif self.__algorithm == "BSplineUniforme":
+                curvas.bsplines(c_points, 1000, bresenham.draw_segment, putpixel)
+            elif self.__algorithm == "BSplineNoUniforme":
+                curvas.bsplines_no_uniforme(c_points, 1000, 3, bresenham.draw_segment, putpixel)
+            elif self.__algorithm == "BezierMenorGrado": 
+                curvas.BezierMenorGrado(c_points, 1000, bresenham.draw_segment, putpixel)
+      
             
 if __name__ == "__main__":
     app = wx.App()
