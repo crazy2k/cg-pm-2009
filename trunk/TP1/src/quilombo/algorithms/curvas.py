c81bf19da846718a5cbe25aa5fedebc3e9d10ca5
martaponzoni <martaponzoni@e377ba90-2068-11de-a7a1-25a91d9625e5>
2009-05-18 05:28:31 +0000

    poly bezier andando

--- trunk/TP1/src/quilombo/algorithms/curvas.py
+++ trunk/TP1/src/quilombo/algorithms/curvas.py
@@ -28,11 +28,10 @@ def Bezier(grafo_control, paso, drawsegment, putpixel):
 
 
 def BezierMenorGrado(grafo_control, paso, drawsegment, putpixel):
-    for i in range ((len(grafo_control)+1)/4):
-        if i==0:
-            Bezier(grafo_control[i*4:i*4+4], paso, drawsegment, putpixel)
-        else:
-            Bezier(grafo_control[i*4-1:i*4+3], paso, drawsegment, putpixel)    
+    copia_grafo_control = copy.deepcopy(grafo_control)
+    while len(copia_grafo_control)>0:
+        Bezier(copia_grafo_control[0:4], paso, drawsegment, putpixel)
+        copia_grafo_control = copia_grafo_control[3:]
 
 
 def bsplines(g, pasos, draw_segment, putpixel):
