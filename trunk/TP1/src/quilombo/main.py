565913c088a303c53e4baca413c01d1e78c4ab68
martaponzoni <martaponzoni@e377ba90-2068-11de-a7a1-25a91d9625e5>
2009-05-20 16:48:48 +0000

    buenisimo !!!!

--- trunk/TP1/src/quilombo/main.py
+++ trunk/TP1/src/quilombo/main.py
@@ -215,7 +215,16 @@ class CurvesWindow(GenericWindow):
             self.__knots.append((len(self.c_points) - self.__grade) + 1)
             
         self.Refresh()
+        wx.StaticText(self, 11, 'Nudo')
+        self.__knotNumber = wx.SpinCtrl(self, 12, pos = (0, 20) , min = 1, max = (len(self.c_points) - self.__grade) , initial = 1)
+        wx.StaticText(self, 13, 'Valor', pos = (0, 50))
+        self.__knotValue = wx.Slider(self, 14, pos = (0, 70), minValue = self.__knots[self.__knotNumber.GetValue() + self.__grade - 2]*100, maxValue = self.__knots[self.__knotNumber.GetValue() + self.__grade]*100)
+        self.Bind(wx.EVT_SLIDER, self.valorNudo, id=14)
 
+    def valorNudo(self, event):
+        self.__knots[self.__knotNumber.GetValue() + self.__grade -1] = float(self.__knotValue.GetValue())/100
+        self.Refresh()
+            
     def menuBezierMenorGrado(self, event):
         self.__algorithm = "BezierMenorGrado"
         self.Refresh()
