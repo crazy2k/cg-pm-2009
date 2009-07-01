3d56b5340b761f90b89201dfe6d344be66f3d55e
pabloa <pabloa@e377ba90-2068-11de-a7a1-25a91d9625e5>
2009-07-01 10:31:54 +0000

    Ahora la planta se puede girar utilizando las flechas del teclado cuando el focus está sobre el canvas.

--- TP3/src/core/glwindows.py
+++ TP3/src/core/glwindows.py
@@ -3,7 +3,8 @@ import wx.glcanvas
 
 from OpenGL.GL import *
 from OpenGL.GLU import *
-from OpenGL.GLUT import *
+
+from math import sin, cos, pi
 
 class GLFrame(wx.Frame):
 
@@ -26,9 +27,18 @@ class BasicGLCanvas(wx.glcanvas.GLCanvas):
         wx.glcanvas.GLCanvas.__init__(self, parent = parent,
             attribList = attrib_list)
 
+        self.context_initialized = False
+
+        self.cam_elevation = 0
+        self.cam_azimuth = 0
+
+        self.radian_unit = pi/180
+
         self.Bind(wx.EVT_SIZE, self.on_size)
         self.Bind(wx.EVT_PAINT, self.on_paint)
         self.Bind(wx.EVT_ERASE_BACKGROUND, self.on_erase_background)
+        self.Bind(wx.EVT_KEY_DOWN, self.on_key_down)
+
 
     def on_size(self, event):
         if self.GetContext():
@@ -41,33 +51,46 @@ class BasicGLCanvas(wx.glcanvas.GLCanvas):
             height = size.GetHeight()
             glViewport(0, 0, width, height)
 
-
-    # XXX: No nos cierra lo del double-buffering. Cuando escribimos y cuando
-    # solo pegamos el buffer?
-    # XXX: Cuales de estas cosas pueden hacerse una unica vez al inicio y
-    # cuales necesitan hacerse en cada on_paint?
     def on_paint(self, event):
+        """Attend the EVT_PAINT event."""
+
         # a PaintDC has to be created when attending an EVT_PAINT event
         wx.PaintDC(self)
 
         # make this canvas' context the current context 
         self.SetCurrent()
 
+        if not self.context_initialized:
+            self.initialize_context()
+
+        # modelview matrix
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        self.position_camera(10, self.cam_elevation, self.cam_azimuth)
+
+        # just see polygons' lines
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL) 
+
+        # clear color and depth buffers
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        self.draw_axis()
+
+        self.draw_tree(5)
+
+        self.SwapBuffers()
+
+    def initialize_context(self):
         # smooth shading: colors of vertices are interpolated
         glShadeModel(GL_SMOOTH)
 
         # specify (0, 0, 0, 1) as clear values (for glClear())
         glClearColor(0, 0, 0, 1)
 
-        # projection matrix will be the identity
+        # projection matrix
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
-        gluPerspective(30, 1.3, 0.01, 1000)
-
-        # modelview matrix will be the identity
-        glMatrixMode(GL_MODELVIEW)
-        glLoadIdentity()
-        gluLookAt(5, 10, 10, 0, 0, 0, 0, 1, 0)
+        gluPerspective(45, 1, 1, 1000)
 
         # enable depth test (otherwise, the depth buffer is not updated)
         glEnable(GL_DEPTH_TEST)
@@ -77,61 +100,81 @@ class BasicGLCanvas(wx.glcanvas.GLCanvas):
         # nicest opt. as quality of color and texture coordinate interpolation
         glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
 
-        # create a new quadrics object
-        #quad = gluNewQuadric()
-        # quadrics rendered with quad will not have texturing
-        #gluQuadricTexture(quad, False)
+        self.context_initialized = True
 
-        # make polygons be filled with color or just see the lines/points
-        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE) 
+    def position_camera(self, radius, elevation, azimuth):
+        """Position the camera at a point P, according to the values for
+        radius, elevation and azimuth given. The center (point the camera is
+        aiming to) is assumed to be the origin (0, 0, 0). Angles should be
+        given in radians.
 
-        # clear color and depth buffers
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        radius      -- distance from the origin to P
+        elevation   -- pi/2 minus the inclination angle (inclination
+                       is the angle between the zenith (0, 1, 0) and the
+                       line formed between the origin and P)
+        azimuth     -- angle between reference direction (0, 0, 1) and
+                       the line from the origin to the projection of P on
+                       the XZ-plane
 
-        # set current color
-        glColor3f(0, 10, 0)
+        """
+        
+        inclination = pi/2 - elevation
+        eye_x = round(radius*cos(azimuth)*sin(inclination))
+        eye_y = round(radius*sin(azimuth)*sin(inclination))
+        eye_z = round(radius*cos(inclination))
 
-        # we save the matrix in the modelview mode for further use
-        #glPushMatrix()
+        print "camera position: ", eye_x, eye_y, eye_z
+        
+        # center (point at which the camera is aiming): always (0, 0, 0)
+        # up vector: (0, 1, 0) (positive Y-axis)
+        gluLookAt(eye_x, eye_y, eye_z, 0, 0, 0, 0, 1, 0)
 
-        # we rotate 10 degrees around the X-axis
-        #angle = -70
-        #glRotated(angle, 1, 0, 0)
-
-        # draw a cylinder
-        #gluCylinder(quad, 0.50, 0.01, 1.0, 26, 4)
-
+    def draw_axis(self):
         glBegin(GL_LINES)
+
+        # X-axis
         glColor3f(1, 0, 0)
+
         glVertex3f(0, 0, 0)
         glVertex3f(1, 0, 0)
+
+        # Y-axis
         glColor3f(0, 1, 0)
+
         glVertex3f(0, 0, 0)
         glVertex3f(0, 1, 0)
+
+        # Z-axis
         glColor3f(0, 0, 1)
         glVertex3f(0, 0, 0)
         glVertex3f(0, 0, 1)
+
         glEnd()
 
+    def draw_tree(self, level):
+        h = 0.5
+        self.draw_cylinder(rad = 0.01, height = h)
+        
+        if level > 0:
 
-        glBegin(GL_POINTS)
-        glColor3f(1, 1, 1)
-        glVertex3f(0, 0, 0)
-        glColor3f(1, 0, 0)
-        glVertex3f(1, 0, 0)
-        glColor3f(0, 1, 0)
-        glVertex3f(0, 1, 0)
-        glColor3f(0, 0, 1)
-        glVertex3f(0, 0, 1)
-        glEnd()
+            glPushMatrix()
 
+            glTranslatef(0, h, 0)
+            glRotatef(15, 0, 0, 1)
 
-        self.draw_tree()
+            self.draw_tree(level - 1)
 
-        # get rid of the new matrix
-        #glPopMatrix()
+            glPopMatrix()
 
-        self.SwapBuffers()
+            glPushMatrix()
+
+            glTranslatef(0, h, 0)
+            glRotatef(-15, 0, 0, 1)
+            glRotatef(-20, 1, 0, 0)
+
+            self.draw_tree(level - 1)
+
+            glPopMatrix()
 
     def draw_cylinder(self, rad, height):
         glPushMatrix()
@@ -148,37 +191,21 @@ class BasicGLCanvas(wx.glcanvas.GLCanvas):
 
         glPopMatrix()
 
-    def draw_tree(self):
-        self.tree(6)
-
-
-    def tree(self, level):
-        h = 0.5
-        self.draw_cylinder(rad = 0.01, height = h)
-        
-        if level > 0:
-
-            glPushMatrix()
-
-            glTranslatef(0, h, 0)
-            glRotatef(15, 0, 0, 1)
-
-            self.tree(level - 1)
-
-            glPopMatrix()
-
-            glPushMatrix()
-
-            glTranslatef(0, h, 0)
-            glRotatef(-15, 0, 0, 1)
-            glRotatef(-20, 1, 0, 0)
-
-            self.tree(level - 1)
-
-            glPopMatrix()
-
-
-
     def on_erase_background(self, event):
         # this is to avoid flickering on Win
         pass
+
+    def on_key_down(self, event):
+        keycode = event.GetKeyCode()
+
+        if keycode == wx.WXK_LEFT:
+            self.cam_elevation += self.radian_unit
+        elif keycode == wx.WXK_RIGHT:
+            self.cam_elevation -= self.radian_unit
+        elif keycode == wx.WXK_UP:
+            self.cam_azimuth += self.radian_unit
+        elif keycode == wx.WXK_DOWN:
+            self.cam_azimuth -= self.radian_unit
+
+        self.Refresh()
+
