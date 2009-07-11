7981520c1e01a8a1798cd959b6e58288cf406854
martaponzoni <martaponzoni@e377ba90-2068-11de-a7a1-25a91d9625e5>
2009-07-11 08:15:23 +0000

    Merge de lo de superficie de revolucion con los nuevos parametros del arbol.

--- trunk/TP3/src/core/glfigures.py
+++ trunk/TP3/src/core/glfigures.py
@@ -1,95 +1,3 @@
-<<<<<<< .mine
-from OpenGL.GL import *
-from OpenGL.GLU import *
-
-from utils.transformations import *
-import random
-
-class SceneNode:
-    
-    def __init__(self, transformation, obj):
-        self.transformation = transformation
-        self.obj = obj
-
-        self.children = []
-
-    def add_child(self, child):
-        self.children.append(child)
-
-    def paint(self):
-
-        glPushMatrix()
-
-        self.apply_transformation()
-
-        self.obj.draw()
-
-        for s in self.children:
-            s.paint()
-
-        glPopMatrix()
-
-    def apply_transformation(self):
-        raise NotImplementedError
-
-
-class GLSceneNode(SceneNode):
-    
-    def __init__(self, transformation, obj):
-        SceneNode.__init__(self, transformation, obj)
-
-    def apply_transformation(self):
-        glMultMatrixd(self.transformation.transpose())
-
-
-def generate_tree(actual_level, height, branch_height, min_cant, max_cant, bottom_radius, top_radius, angle, transformation, generate_trunk):
-    # generate a node with a new trunk into it, and make
-    # initial_transformation its associated transformation
-    
-    trunk = generate_trunk(bottom_radius, top_radius, branch_height)
-    node = GLSceneNode(transformation, trunk)
-    
-    if actual_level < height:
-        cant = int(random.random()*float(max_cant - min_cant)) + min_cant
-        for i in range (cant):
-                angle_x = int(random.random()*float(2*angle) - float(angle))
-                angle_z = int(random.random()*float(2*angle) - float(angle))
-                next_transformation = translation(trunk.endpoint()) * rotation(degree2radians(angle_z), "Z") * rotation(degree2radians(angle_x), "X")
-                node.add_child(generate_tree(actual_level + 1, height, branch_height, min_cant, max_cant, 2*bottom_radius - top_radius, bottom_radius, angle, next_transformation, generate_trunk))
-    return node
-
-class GLCylinder:
-
-    def __init__(self, bottom_radius, top_radius, height):
-        self.bottom_radius = bottom_radius
-        self.top_radius = top_radius
-        self.height = height
-                
-    def draw(self):
-        glPushMatrix()
-
-        # create a new quadrics object
-        quad = gluNewQuadric()
-        # quadrics rendered with quad will not have texturing
-        gluQuadricTexture(quad, False)
-        
-        glRotatef(-90, 1, 0, 0)
-        gluCylinder(quad, self.top_radius, self.bottom_radius, self.height, 26, 4)
-        
-        gluDeleteQuadric(quad)
-
-        glPopMatrix()
-
-    def endpoint(self):
-        return (0, self.height, 0)
-
-    @classmethod
-    def generate(cls, bottom_radius, top_radius, height):
-        c = GLCylinder(bottom_radius, top_radius, height)
-        return c
-
-
-=======
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
@@ -142,40 +50,31 @@ class GLSceneNode(SceneNode):
     def apply_transformation(self):
         glMultMatrixd(self.transformation.transpose())
 
-
-def generate_tree(actual_level, levels, radius, radius_diff, initial_transformation, generate_trunk):
+        
+def generate_tree(actual_level, height, branch_height, min_cant, max_cant, bottom_radius, top_radius, angle, transformation, generate_trunk):
     # generate a node with a new trunk into it, and make
     # initial_transformation its associated transformation
-    if actual_level == 0:
-        trunk = generate_trunk(radius*1.5, 1.4, actual_level, radius_diff)
-    else:
-        trunk = generate_trunk(radius - actual_level*0.01, 0.7 - 0.15*actual_level, actual_level, radius_diff)
-    node = GLSceneNode(initial_transformation, trunk)
     
-    if actual_level < levels:
-        cant = (int(random.random()*100)%3)+ levels - 2*actual_level + 4
-        create_branchs(cant, actual_level, levels, radius, radius_diff, generate_trunk, trunk, node)
+    trunk = generate_trunk(bottom_radius, top_radius, branch_height)
+    node = GLSceneNode(transformation, trunk)
+    
+    if actual_level < height:
+        cant = int(random.random()*float(max_cant - min_cant)) + min_cant
+        for i in range (cant):
+                angle_x = int(random.random()*float(2*angle) - float(angle))
+                angle_z = int(random.random()*float(2*angle) - float(angle))
+                next_transformation = translation(trunk.endpoint()) * rotation(degree2radians(angle_z), "Z") * rotation(degree2radians(angle_x), "X")
+                node.add_child(generate_tree(actual_level + 1, height, branch_height, min_cant, max_cant, 2*bottom_radius - top_radius, bottom_radius, angle, next_transformation, generate_trunk))
     return node
+
     
-def create_branchs(cant, actual_level, levels, radius, radius_diff, generate_trunk, trunk, node):
-    for i in range(cant):
-        create_branch(actual_level + 1, levels, radius, radius_diff, generate_trunk, trunk, node)
-
-def create_branch(level, levels, radius, diff, generate_trunk, trunk, node):
-    r = ((random.random()*180)%90)-45
-    s = ((random.random()*260)%130)-65
-    translation_m = translation(trunk.endpoint())
-    next_t = translation_m * rotation(degree2radians(r), "Z") * rotation(degree2radians(s), "X")
-    node.add_child(generate_tree(level, levels, radius, diff, next_t, generate_trunk))
-
-
 class GLCylinder:
 
-    def __init__(self, radius, diff, height):
-        self.radius = radius
+    def __init__(self, bottom_radius, top_radius, height):
+        self.bottom_radius = bottom_radius
+        self.top_radius = top_radius
         self.height = height
-        self.radius_diff = diff
-        
+                
     def draw(self):
         glPushMatrix()
 
@@ -185,7 +84,7 @@ class GLCylinder:
         gluQuadricTexture(quad, False)
         
         glRotatef(-90, 1, 0, 0)
-        gluCylinder(quad, self.radius, self.radius - self.radius_diff, self.height, 26, 4)
+        gluCylinder(quad, self.top_radius, self.bottom_radius, self.height, 26, 4)
         
         gluDeleteQuadric(quad)
 
@@ -195,8 +94,8 @@ class GLCylinder:
         return (0, self.height, 0)
 
     @classmethod
-    def generate(cls, radius, height, level, diff):
-        c = GLCylinder(radius, diff, height)
+    def generate(cls, bottom_radius, top_radius, height):
+        c = GLCylinder(bottom_radius, top_radius, height)
         return c
 
 
@@ -291,13 +190,7 @@ class GLSurfaceOfRevolution:
         return (0, self.function(1)[1], 0)
 
     @classmethod
-    def generate(cls, radius, height, level, diff):
-        function = lambda x: (radius, x*height)
-        c = GLSurfaceOfRevolution(function, 1, 8)
+    def generate(cls, bottom_radius, top_radius, height):
+        function = lambda x: (bottom_radius, x*height)
+        c = GLSurfaceOfRevolution(function, 1, 25)
         return c
-
-
-
-
-
->>>>>>> .r104
