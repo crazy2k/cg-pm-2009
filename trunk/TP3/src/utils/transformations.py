ae18fe2d586ce732d3e522a7a6d08e88c70d2af9
pabloa <pabloa@e377ba90-2068-11de-a7a1-25a91d9625e5>
2009-07-19 01:32:04 +0000

    Superficie de barrido andando. Tiene algunos quirks al girar y ver el arbol de atras.

--- trunk/TP3/src/utils/transformations.py
+++ trunk/TP3/src/utils/transformations.py
@@ -1,23 +1,26 @@
 
 from math import sin, cos, pi
-from numpy import identity, matrix
+from numpy import identity, matrix, float64
 
-IDENTITY_3 = identity(3, float)
-IDENTITY_4 = identity(4, float)
+IDENTITY_3 = identity(3, float64)
+IDENTITY_4 = identity(4, float64)
+
+def my_matrix(data):
+    return matrix(data, float64)
 
 def translation(xyz):
     x, y, z = xyz
-    return matrix([[1, 0, 0, x],
+    return my_matrix([[1, 0, 0, x],
                    [0, 1, 0, y],
                    [0, 0, 1, z],
-                   [0, 0, 0, 1]], float)
+                   [0, 0, 0, 1]])
 
 def rotation(a, axis):
     if axis == 'X':
-        return matrix([[1, 0, 0, 0],
+        return my_matrix([[1, 0, 0, 0],
                        [0, cos(a), -sin(a), 0],
                        [0, sin(a), cos(a), 0],
-                       [0, 0, 0, 1]], float)
+                       [0, 0, 0, 1]])
     elif axis == 'Y':
         return matrix([[cos(a), 0, sin(a), 0],
                        [0, 1, 0, 0],
@@ -25,10 +28,10 @@ def rotation(a, axis):
                        [0, 0, 0, 1]], float)
 
     elif axis == 'Z':
-        return matrix([[cos(a), -sin(a), 0, 0],
+        return my_matrix([[cos(a), -sin(a), 0, 0],
                        [sin(a), cos(a), 0, 0],
                        [0, 0, 1, 0],
-                       [0, 0, 0, 1]], float)
+                       [0, 0, 0, 1]])
 
 def degree2radians(degree):
     return degree*pi/180
@@ -37,13 +40,13 @@ def twodseq_to_4x1vector(seq):
     """Transform a two-dimensional sequence into a 4x1 matrix. The third
     coordinate is set to 0, and the fourth to 1."""
     point = (seq[0], seq[1], 0, 1)
-    m = matrix(point)
+    m = my_matrix(point)
     return m.transpose()
 
 def threedseq_to_4x1vector(seq):
     """Transform a three-dimensional sequence into a 4x1 matrix. The fourth
     coordinate is set 1."""
     point = (seq[0], seq[1], seq[2], 1)
-    m = matrix(point)
+    m = my_matrix(point)
     return m.transpose()
 
