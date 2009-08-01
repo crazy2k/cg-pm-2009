5913303200a3de283bd73f20f61c9fbf0eedf5e9
pabloa <pabloa@e377ba90-2068-11de-a7a1-25a91d9625e5>
2009-08-01 01:51:44 +0000

    Avanza la interfaz de usuario.

--- TP3/src/core/glwindows.py
+++ TP3/src/core/glwindows.py
@@ -13,28 +13,40 @@ from utils.transformations import IDENTITY_4
 
 class GLFrame(wx.Frame):
 
-    def __init__(self, id, title, size):
-        wx.Frame.__init__(self, None, id, title, size = size)
+    def __init__(self, id, title):
+        wx.Frame.__init__(self, None, id, title)
 
+        #
+        # panel loading from XRC
+        # 
+        res = xrc.XmlResource("view/panel.xrc")
+        self.panel = res.LoadPanel(self, "ID_WXPANEL")
+
+        panel_w, panel_h = self.panel.GetSize().Get()
+
+        self.bind_panel_events(res)
+
+        #
         # DrawingGLCanvas' creation
+        #
         attrib_list = (wx.glcanvas.WX_GL_RGBA, # RGBA
                        wx.glcanvas.WX_GL_DOUBLEBUFFER) # double-buffering
 
-        glcanvas_size = wx.Size(450, 450)
+        # size settings
+        glcanvas_w = 500
+        glcanvas_h = panel_h
+        
+        glcanvas_size = wx.Size(glcanvas_w, glcanvas_h)
 
+        # creation itself
         self.glcanvas = DrawingGLCanvas(self, attrib_list, glcanvas_size)
 
-        generate_surface = GLCylinder.generate_trunk
+        # the canvas is filled
+        self.fill_canvas()
 
-        primary_values = {"branch_height":1.2, "min_cant":6, "max_cant":7, "initial_radius":0.06, "radius_diff":0.01, "angle":45}
-        secondary_values = {"branch_height":0.5, "min_cant":2, "max_cant":4, "initial_radius":0.04, "radius_diff":0.01, "angle":40}
-        tertiary_values = {"branch_height":0.2, "min_cant":1, "max_cant":3, "initial_radius":0.03, "radius_diff":0.005, "angle":35}
-        
-        self.glcanvas.add_figure(generate_tree(0, 4, primary_values, secondary_values, tertiary_values, IDENTITY_4, generate_surface, generate_surface))
+        # fill the panel with canvas' state
+        self.fill_panel()
 
-        # panel is loaded
-        self.res = xrc.XmlResource("view/panel.xrc")
-        self.panel = self.res.LoadPanel(self, "ID_WXPANEL")
 
         # sizer's creation
         grid = wx.FlexGridSizer(rows = 0, cols = 2, vgap = 0, hgap = 0)
@@ -42,9 +54,85 @@ class GLFrame(wx.Frame):
         grid.Add(self.panel)
 
         # frame initialization
+        self.SetSize(wx.Size(glcanvas_w + panel_w, panel_h))
         self.SetSizer(grid)
         self.Centre()
         self.Show(True)
+
+    def bind_panel_events(self, res):
+        self.Bind(wx.EVT_CHECKBOX, self.on_check_perspective,
+            id = xrc.XRCID("ID_CHECKBOX_PERSPECTIVE"))
+
+    def on_check_perspective(self, event):
+        old_value = self.glcanvas.perspective_projection_enabled
+        self.glcanvas.perspective_projection_enabled = not old_value
+
+        self.glcanvas.Refresh()
+
+    def fill_panel(self):
+        def set_boxes_values(boxes, values):
+            length = len(boxes)
+
+            if length != len(values):
+                raise Error("Number of boxes and number of values must match.")
+            for i in range(length):
+                box = xrc.XRCCTRL(self, boxes[i])
+                box.SetValue(values[i])
+
+        c = self.glcanvas
+
+        checkbox_perspective = xrc.XRCCTRL(self,
+            "ID_CHECKBOX_PERSPECTIVE")
+
+        state = c.perspective_projection_enabled
+        checkbox_perspective.SetValue(state)
+
+        boxes = ("ID_PERSPECTIVE_FOVY", "ID_PERSPECTIVE_ASPECT",
+            "ID_PERSPECTIVE_ZNEAR", "ID_PERSPECTIVE_ZFAR")
+        values = (c.perspective_projection_fovy,
+            c.perspective_projection_aspect,
+            c.perspective_projection_zNear,
+            c.perspective_projection_zFar)
+        set_boxes_values(boxes, values)
+
+    
+    def fill_canvas(self):
+        generate_surface = GLCylinder.generate_trunk
+
+        primary_values = {
+            "branch_height": 1.2,
+            "min_cant": 6,
+            "max_cant": 7,
+            "initial_radius": 0.06,
+            "radius_diff": 0.01,
+            "angle": 45
+        }
+        secondary_values = {
+            "branch_height": 0.5,
+            "min_cant": 2,
+            "max_cant": 4,
+            "initial_radius": 0.04,
+            "radius_diff": 0.01,
+            "angle":40
+        }
+        tertiary_values = {
+            "branch_height": 0.2,
+            "min_cant": 1,
+            "max_cant": 3,
+            "initial_radius": 0.03,
+            "radius_diff": 0.005,
+            "angle":35
+        }
+        
+        self.glcanvas.add_figure(
+            generate_tree(
+                0, 4, primary_values, secondary_values,
+                tertiary_values, IDENTITY_4, generate_surface,
+                generate_surface
+            )
+        )
+
+
 
 class DrawingGLCanvas(wx.glcanvas.GLCanvas):
     """A DrawingGLCanvas is basically a GLCanvas with some additional
@@ -74,10 +162,30 @@ class DrawingGLCanvas(wx.glcanvas.GLCanvas):
 
         self.radian_unit = pi/180
 
+        # the canvas has its default state when created
+        self.restore_default_state()
+
         self.Bind(wx.EVT_SIZE, self.on_size)
         self.Bind(wx.EVT_PAINT, self.on_paint)
         self.Bind(wx.EVT_ERASE_BACKGROUND, self.on_erase_background)
         self.Bind(wx.EVT_KEY_DOWN, self.on_key_down)
+
+    def restore_default_state(self):
+        self.perspective_projection_enabled = ppe = True
+        self.perspective_projection_fovy = 45
+        self.perspective_projection_aspect = 1
+        self.perspective_projection_zNear = 0.1
+        self.perspective_projection_zFar = 1000
+
+        self.ortho_projection_enabled = not ppe
+        self.ortho_projection_left = 0
+        self.ortho_projection_right = 1
+        self.ortho_projection_bottom = 0
+        self.ortho_projection_top = 1
+        self.ortho_projection_nearVal = 0.1
+        self.ortho_projection_farVal = 1000
+
+
 
     def add_figure(self, figure):
         self.figures.append(figure)
@@ -93,8 +201,6 @@ class DrawingGLCanvas(wx.glcanvas.GLCanvas):
             height = size.GetHeight()
             glViewport(0, 0, width, height)
 
-            print "halloa!"
-
     def on_paint(self, event):
 
         # a PaintDC has to be created when attending an EVT_PAINT event
@@ -105,6 +211,15 @@ class DrawingGLCanvas(wx.glcanvas.GLCanvas):
 
         if not self.context_initialized:
             self.initialize_context()
+
+        # projection matrix
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        if self.perspective_projection_enabled:
+            gluPerspective(self.perspective_projection_fovy,
+                self.perspective_projection_aspect,
+                self.perspective_projection_zNear,
+                self.perspective_projection_zFar)
 
         # modelview matrix
         glMatrixMode(GL_MODELVIEW)
@@ -131,11 +246,7 @@ class DrawingGLCanvas(wx.glcanvas.GLCanvas):
         # specify (0, 0, 0, 1) as clear values (for glClear())
         glClearColor(0, 0, 0, 1)
 
-        # projection matrix
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-        gluPerspective(45, 1, 0.1, 1000)
-
+        
         # enable depth test (otherwise, the depth buffer is not updated)
         glEnable(GL_DEPTH_TEST)
         # pixel passes if "incoming depth <= stored depth"
